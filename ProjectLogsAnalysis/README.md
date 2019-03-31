6180ee46c00d13f7f93ac0aaff6204e5d1aefdc1
vagrant <vagrant@vagrant.vm>
2019-03-31 14:03:08 +0000

    Initial delivery of LogsAnalysis project.

---
+++ ProjectLogsAnalysis/README.md
@@ -0,0 +1,47 @@
+### Project: Logs Analysis
+
+The structure of the script is quite straight forward, there are three functions that retreive the answers to the questions.  
+
+These views are created within the python script and are here from reference.  
+```
+        CREATE VIEW error_view AS
+        SELECT date_trunc('day', time) "day", count(*) AS requests
+        FROM log
+        WHERE status LIKE '404%'
+        GROUP BY day;
+```
+```
+        CREATE VIEW request_view AS
+        SELECT date_trunc('day', time) "day", count(*) AS requests
+        FROM log
+        GROUP BY day;
+```
+
+### How to run
+
+Load up and check the database:  
+`psql -d news -f newsdata.sql`
+`psql -d news`
+`psql`
+Execute the analysis script:  
+`$ python logs_analysis.py`
+
+### Output sample
+```
+
+Project: Log Analysis
+
+1. What are the most popular three articles of all time?
+"Candidate is jerk, alleges rival" @ 338647 views
+"Bears love berries, alleges bear" @ 253801 views
+"Bad things gone, say good people" @ 170098 views
+
+2. Who are the most popular article authors of all time?
+"Ursula La Multa" @ 507594 views
+"Rudolf von Treppenwitz" @ 423457 views
+"Anonymous Contributor" @ 170098 views
+
+3. On which days did more than 1% of requests lead to errors?
+July 17, 2016 - 2.3% errors
+
+```
---
+++ ProjectLogsAnalysis/logs_analysis.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python3
+
+import psycopg2
+
+DATABASE_NAME = "news"
+
+
+def execute_query_string(query_string):
+    db = psycopg2.connect('dbname=' + DATABASE_NAME)
+    cur = db.cursor()
+    cur.execute(query_string)
+    res = cur.fetchall()
+    db.close()
+    return res
+
+
+def most_popular_articles():
+    """Most popular three articles of all time"""
+
+    query_string = """
+        SELECT title, count(log.id) AS view
+        FROM articles, log
+        WHERE log.path = CONCAT('/article/', articles.slug)
+        GROUP BY articles.title
+        ORDER BY view DESC
+        LIMIT 3;
+    """
+
+    # Execute query string
+    results = execute_query_string(query_string)
+
+    # Present results
+    print('1. What are the most popular three articles of all time?')
+    for r in results:
+        print("\"" + r[0] + "\" @ " + str(r[1]) + " views")
+
+
+def most_popular_article_authors():
+    """returns top 3 most popular authors"""
+
+    query_string = """
+        SELECT name, count(log.id) AS view
+        FROM authors
+        JOIN articles
+        ON authors.id = author
+        JOIN log
+        ON path LIKE concat('/article/%', articles.slug)
+        GROUP BY name
+        ORDER BY view DESC
+        LIMIT 3;
+    """
+
+    # Execute query string
+    results = execute_query_string(query_string)
+
+    # Present results
+    print('2. Who are the most popular article authors of all time?')
+    for r in results:
+        print("\"" + r[0] + "\" @ " + str(r[1]) + " views")
+
+
+def days_with_requests_erros():
+    """returns days with more than 1% errors"""
+
+    query_string = """
+        CREATE VIEW error_view AS
+        SELECT date_trunc('day', time) "day", count(*) AS requests
+        FROM log
+        WHERE status LIKE '404%'
+        GROUP BY day;
+
+        CREATE VIEW request_view AS
+        SELECT date_trunc('day', time) "day", count(*) AS requests
+        FROM log
+        GROUP BY day;
+
+        SELECT total.day, ROUND(((errors.requests*1.0) / total.requests), 4)
+        AS percent
+        FROM error_view AS errors
+        JOIN request_view AS total
+        ON total.day = errors.day
+        WHERE (ROUND(((errors.requests*1.0) / total.requests), 4) > 0.01)
+        ORDER BY percent DESC;
+    """
+
+    # Execute query string
+    results = execute_query_string(query_string)
+
+    # Present results
+    print('3. On which days did more than 1% of requests lead to errors?')
+    for r in results:
+        date = r[0].strftime('%B %d, %Y')
+        error = str(round(r[1] * 100, 1)) + "%" + " errors"
+        print(date + " - " + error)
+
+
+if __name__ == "__main__":
+    print()
+    print('Project: Log Analysis')
+    print()
+    most_popular_articles()
+    print()
+    most_popular_article_authors()
+    print()
+    days_with_requests_erros()
+    print()
